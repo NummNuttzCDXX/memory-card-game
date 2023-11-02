90b1f91fe8202116e5ca1422fad06a1f1c1e28ea
NummNuttzCDXX <bbirdy2018@gmail.com>
2023-11-02 19:35:54 -0400

    Update Card: Add effect: Process img data on render to get the best fit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,22 @@
 /* eslint-disable react/prop-types */
 // Card component
+import {useEffect} from 'react';
 import '../styles/Card.css';
+import MarvelData from '../modules/marvelData';
 
 export default function Card({charData}) {
+	// Calculate img size on every render
+	useEffect(() => {
+		const imgData = MarvelData.processImg(charData.img);
+		charData.img = imgData;
+	});
+
+	// Get URL
+	const imgUrl = charData.img.path + charData.img.size + charData.img.ext;
+
 	return (
 		<div className="card">
-			<img src={!charData ? '' : charData.img} alt={charData.name + ' image'} />
+			<img src={imgUrl} alt={charData.name + ' image'} />
 			<p> {!charData ? '' : charData.name} </p>
 		</div>
 	);
